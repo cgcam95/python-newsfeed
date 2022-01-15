83e31bd0b4dc8ba96ccc946d695a5cd746eb5895
Camil Grabowski <camil.grabowski@uconn.edu>
2022-01-15 14:43:07 -0500

    set up user login and sessions

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -9,10 +9,12 @@ def index():
   # get all posts
   db = get_db()
   posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
   return render_template(
   'homepage.html',
-  posts=posts
-  )
+  posts=posts,
+  loggedIn=session.get('loggedIn')
+)
 
 @bp.route('/post/<id>')
 def single(id):
@@ -22,11 +24,21 @@ def single(id):
 
   # render single post template
   return render_template(
-    'single-post.html',
-    post=post
-    ) 
+  'single-post.html',
+  post=post,
+  loggedIn=session.get('loggedIn')
+) 
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  # not logged in yet
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+
+  return redirect('/dashboard')
+
+
+
+
+
 
